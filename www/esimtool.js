beee21c67d8220988f98876a05bc17db70dfc64b
joaogdomingos12 <joaodomingos@deloitte.pt>
2019-10-07 16:12:34 +0100

    ToastyCommit

---
+++ www/esimtool.js
@@ -0,0 +1,21 @@
+// Empty constructor
+function eSimTool() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+eSimTool.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'eSimTool', 'show', [options]);
+}
+
+// Installation constructor that binds eSimTool to window
+eSimTool.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.eSimTool = new eSimTool();
+  return window.plugins.eSimTool;
+};
+cordova.addConstructor(eSimTool.install);
